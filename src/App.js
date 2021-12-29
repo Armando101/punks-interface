523474669fd1e1e08d92a9372deb8d08bb7dc654
Armando Rivera <rivera.armando997@gmail.com>
2021-12-28 18:34:46 -0600

    feat(App.js): manage metamask conection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { Route } from "react-router-dom";
+import Web3 from "web3";
 import Home from "./views/home";
 
 function App() {
+  useEffect(() => {
+    if (window.ethereum) {
+      // window.ethereum
+      //   .request({ method: "eth_requestAccounts" })
+      //   .then(console.log);
+      const web3 = new Web3(window.ethereum);
+      web3.eth.requestAccounts().then(console.log);
+    }
+  }, []);
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Route path="/" element={<Home />} />
     </>
   );
 }
