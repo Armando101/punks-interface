ed43dfb8902fb23cf1bc8199af5c9adaefc5af1c
Armando Rivera <rivera.armando997@gmail.com>
2021-12-28 19:00:19 -0600

    feat(layout): layout initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import { useEffect } from "react";
 import { Route } from "react-router-dom";
-import Web3 from "web3";
+import MainLayout from "./layouts/main";
 import Home from "./views/home";
 
 function App() {
-  useEffect(() => {
-    if (window.ethereum) {
-      // window.ethereum
-      //   .request({ method: "eth_requestAccounts" })
-      //   .then(console.log);
-      const web3 = new Web3(window.ethereum);
-      web3.eth.requestAccounts().then(console.log);
-    }
-  }, []);
-
   return (
-    <>
-      <Route path="/" element={<Home />} />
-    </>
+    <MainLayout>
+      <Route path="/" component={Home} />
+    </MainLayout>
   );
 }
 
